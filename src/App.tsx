ae947be7a06da4b4c9053c698f98de6b5a0d9dbc
吴健 <wx_a756ba7fab0c423fa663e576c7601c86@git.code.tencent.com>
2021-05-24 09:20:42 +0800

    ✨ feat: 新增message组件(need fix bug)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,42 @@
 /*
  * @Author: Indexsarrol
  * @Date: 2021-04-23 10:15:37
- * @LastEditTime: 2021-05-21 10:36:24
+ * @LastEditTime: 2021-05-21 18:00:09
  * @LastEditors: Indexsarrol
  * @Description: 
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tabs from './components/Tabs/Tabs';
 import TabPane from './components/Tabs/TabPane';
 import Button from './components/Button/Button';
+import message from './components/Message/Message';
+
 import './App.css';
-const App: React.FC = () => (
+
+
+const App: React.FC = () => {
+
+
+  const alertMessage = () => {
+    message.success('123', 1000, () => {
+      console.log('close');
+    })
+  }
+  const alertMessage2 = () => {
+    message.success('456', 1000, () => {
+      console.log('close');
+    })
+  }
+
+  
+  return (
     <div className="App">
       <div style={{ width: 600, marginTop: 20 }}>
-        <Tabs 
-          type="line" 
-          defaultKey="0" 
-          tabPosition="top" 
-          tabBarExtraContent={<Button>增加</Button>}
-          onChange={(key) => {console.log(key)}}
-        >
-          <TabPane key="0" tab="选项卡一">选项卡一内容</TabPane>
-          <TabPane key="1" tab="选项卡二">选项卡二内容</TabPane>
-          <TabPane key="2" tab="选项卡三" disabled>选项卡三内容</TabPane>
-          <TabPane key="3" tab="选项卡四">选项卡四内容</TabPane>
-        </Tabs>
+        <Button onClick={alertMessage}>消息提示框</Button>
+        <Button onClick={alertMessage2}>消息提示框</Button>
       </div>
     </div>
   );
+}
 
 export default App;
--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -3,14 +3,15 @@
  * @Author: Indexsarrol
  * @Date: 2021-05-13 15:02:30
  * @LastEditors: Indexsarrol
- * @LastEditTime: 2021-05-13 15:22:41
+ * @LastEditTime: 2021-05-21 16:15:46
  */
 
 
 import React from 'react';
 import classnames from 'classnames';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
 library.add(fas);
