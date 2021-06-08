922a8ab58ab56678480a2eb76e67eda5a32ba118
吴健 <wx_a756ba7fab0c423fa663e576c7601c86@git.code.tencent.com>
2021-06-08 17:57:34 +0800

    新增button组件stories

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: Indexsarrol
+ * @Date: 2021-06-02 16:04:26
+ * @LastEditors: Indexsarrol
+ * @LastEditTime: 2021-06-08 16:04:13
+ */
+module.exports = {
+  "stories": [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
