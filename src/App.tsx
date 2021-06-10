7a0c6ecea9dc1b60564bb0f6ec1af3b9127dd828
吴健 <wx_a756ba7fab0c423fa663e576c7601c86@git.code.tencent.com>
2021-06-10 09:25:42 +0800

    新增Empty组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Indexsarrol
  * @Date: 2021-04-23 10:15:37
- * @LastEditTime: 2021-06-02 14:09:07
+ * @LastEditTime: 2021-06-09 17:53:17
  * @LastEditors: Indexsarrol
  * @Description: 
  */
@@ -11,56 +11,17 @@ import notification from './components/Notification/Notification';
 import message from './components/Message/Message';
 import Icon from './components/Icon/icon';
 import Switch from './components/Switch/Switch';
+import Empty from './components/Empty/Empty';
 import './App.css';
 
 
 const App: React.FC = () => {
-  const [value, setValue] = useState('');
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-  const alertSuccessNotification = () => {
-    message.open({
-      type: "warning",
-      title: 'this is a message',
-      duration: 3,
-    })
-  }
 
-  const alertErrorNotification = () => {
-    notification.open({
-      type: "error",
-      duration: 0,
-      message: 'Notification Title',
-      description: ' will never close automatically. I will be close automatically. I will never close automatically.',
-    });
-  }
-
-  const alertMessage = () => {
-
-  }
 
   return (
     <div className="App">
       <div style={{ width: 400, marginTop: 20 }}>
-        <Button onClick={alertSuccessNotification} >消息通知（成功）</Button>
-        <Button onClick={alertErrorNotification} >消息通知（失败）</Button>
-        <Button onClick={alertMessage}>通知</Button>
-        <Switch
-          size="large"
-          checkedText="开" 
-          unCheckedText="关"
-          onChange={(checked,e) => { console.log(checked, e) }}
-        />
-
-        <Switch
-          checkedText="开" 
-          unCheckedText="关"
-        />
-
-        <Switch
-          size="small"
-        />
+        <Empty />
       </div>
     </div>
   );
